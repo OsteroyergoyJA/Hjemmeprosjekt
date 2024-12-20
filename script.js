27b4ce588937ba23ebbe3c67794f393049d9fcbc
OsteroyergoyJA <oosteroyergoy@gmail.com>
2024-12-20 22:00:54 +0100

    Hjemmeprosjekt

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('Hello!');
+
+let boxIndex = 1;
+
+function showBoxes(n) {
+  let i;
+  let boxes = document.getElementsByClassName("boxes");
+  
+  if (n > boxes.length) { boxIndex = 1; }
+  if (n < 1) { boxIndex = boxes.length; }
+  
+  for (i = 0; i < boxes.length; i++) {
+    boxes[i].style.display = "none";
+  }
+  
+  boxes[boxIndex - 1].style.display = "block";
+}
+
+document.querySelector(".prev").addEventListener("click", function() {
+  showBoxes(boxIndex -= 1);
+});
+
+document.querySelector(".next").addEventListener("click", function() {
+  showBoxes(boxIndex += 1);
+});
+
+showBoxes(boxIndex);
+
+/* Hvorfor må det være let? */
+
+
