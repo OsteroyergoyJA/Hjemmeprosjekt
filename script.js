1195efbc0bfd0eea74b495a842617a106ba6c77f
OsteroyergoyJA <oosteroyergoy@gmail.com>
2024-12-21 12:18:24 +0100

    Forsok på å lagre arrayfunksjon

--- script.js
+++ script.js
@@ -25,44 +25,37 @@ document.querySelector(".next").addEventListener("click", function() {
 
 showBoxes(boxIndex);
 
- /*Hvorfor må det være let? */
+/* Hvorfor må det være let? */
 
 let objectArray = [
   {car: "skoda", isGood: true},
-  {car: "volkswage", isGood: true},
+  {car: "volkswagen", isGood: true},
   {car: "audi", isGood: false},
 ]
 
-let cars = objectArray.map(function (cars) {
- return cars.car;
-});
 
-console.log("first car type:", objectArray[0].car);
-
-
-let cartype = objectArray.map((car) => car.car);
-
-/*Hvorfor car.car, trodde det skulle være cars.car */
+let cars = objectArray.map((car) => car.car);
 
 let i = 0;
 
 function displayCars() {
-  if (i < cartype.length) {
+  if (i < cars.length) {
     let displayContent = `
-      <p>Car 1: ${cartype[i]}</p>
-      <p>Car 2: ${cartype[i + 1]}</p>
-    `;
-    
+    <p> Car1: ${cars[i]}</p>
+    <p> Car2: ${cars[i + 1]}</p>`;
     document.getElementsByClassName('sections')[0].innerHTML = displayContent;
-    i += 2;
-  } else {
-    clearInterval(intervalId);
-    document.getElementsByClassName('sections')[0].innerHTML =
-      '<p>No more cars to show.</p>';
+    i +=2;
   }
+
+  else {
+    clearInterval(intervalId);
+    codument.getElementsByClassName('sections')[0].innerHTML = '<p> No more cars to show.</p>';
+  }
+
 }
 
 
 let intervalId = setInterval(displayCars, 6000);
 
 displayCars();
+
