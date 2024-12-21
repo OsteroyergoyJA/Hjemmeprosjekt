975a87207385143e28ff25a7d21ca548fb289511
OsteroyergoyJA <oosteroyergoy@gmail.com>
2024-12-21 08:18:49 +0100

    Hentet inn funksjon og array

--- script.js
+++ script.js
@@ -27,4 +27,41 @@ showBoxes(boxIndex);
 
 /* Hvorfor må det være let? */
 
+let objectArray = [
+  {car: "skoda", isGood: true},
+  {car: "volkswage", isGood: true},
+  {car: "audi", isGood: false},
+]
 
+let cars = objectArray.map(function (cars) {
+ return cars.car;
+});
+
+console.log("first car type:", objectArray[0].car);
+
+
+
+/* let names = arrayDetails.map((person) => person.name);
+
+let i = 0;
+
+function displayNames() {
+  if (i < names.length) {
+    let displayContent = `
+    <p>Name 1: ${names[i]}</p>
+    <p>Name 2: ${names[i + 1]}</p>
+`;
+
+    document.getElementById('names-list').innerHTML = displayContent;
+    i += 2;
+  } else {
+    clearInterval(intervalId);
+    document.getElementById('names-list').innerHTML =
+      '<p>No more names to show.</p>';
+  }
+}
+
+let intervalId = setInterval(displayNames, 6000);
+
+displayNames();
+*/ 
